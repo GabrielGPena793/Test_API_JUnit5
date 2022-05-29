6d2b3ff2f4e13d47cff3afe3e17e40200cf3628d
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2022-05-29 01:55:06 -0300

    Create project and Class User

---
+++ src/main/java/br/com/uldemy/api/model/User.java
@@ -0,0 +1,25 @@
+package br.com.uldemy.api.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
