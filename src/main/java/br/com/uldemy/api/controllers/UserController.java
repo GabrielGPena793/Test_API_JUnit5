c961d788e8931b298b317eed5cd244da66f4249e
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2022-05-29 16:57:04 -0300

    class User resource findById

--- src/main/java/br/com/uldemy/api/controllers/UserController.java
+++ src/main/java/br/com/uldemy/api/controllers/UserController.java
@@ -1,6 +1,8 @@
 package br.com.uldemy.api.controllers;
 
 import br.com.uldemy.api.model.User;
+import br.com.uldemy.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(new User(1, "Gabriel", "ghabriel@hotmcail.com", "dadasdas"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
