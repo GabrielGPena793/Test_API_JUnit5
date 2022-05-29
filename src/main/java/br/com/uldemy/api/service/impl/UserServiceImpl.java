e695edd220e810006ad39327bb5f3a8012488667
gabrielgpena793 <kbeca.gabriel793@gmail.com>
2022-05-29 16:56:44 -0300

    class User service impl findById

---
+++ src/main/java/br/com/uldemy/api/service/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package br.com.uldemy.api.service.impl;
+
+import br.com.uldemy.api.model.User;
+import br.com.uldemy.api.repositories.UserRepsitory;
+import br.com.uldemy.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepsitory repsitory;
+
+    @Override
+    public User findById(Integer id) {
+
+        Optional<User> userModel = repsitory.findById(id);
+
+        return userModel.orElse(null);
+    }
+
+}
